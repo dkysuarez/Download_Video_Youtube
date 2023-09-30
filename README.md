3cebf4fd537195138cf05b59bc8bb723b15694e9
dkysuarez <dkysuarez1@gmail.com>
2023-09-30 11:21:01 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Download_Video_Youtube
+# Introduction
+This project is a tool for downloading YouTube videos in the highest possible quality. The application is easy to use and designed to work on any operating system.
+
+## How to use
+1. Open the application on your computer.
+2. Copy the URL of the YouTube video you want to download.
+3. Press the download button and wait for the process to complete
+
+
+## Installing the dependencies
+pip -r requirements.txt
+
+## Technologies used
+flask
+pytube 
+
+ 
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pytube import YouTube
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    try:
+        video = YouTube(url)
+        video.streams.get_highest_resolution().download()
+        return redirect(url_for('success'))
+    except Exception as e:
+        return redirect(url_for('error'))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
